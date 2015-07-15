0381cca480f076e4483d8a7414f6297258502a49
Matthew Saul <matthewsaul@Matthews-MacBook-Pro.local>
2015-07-14 20:36:06 -0700

    added profile.js and completed the project

--- app.js
+++ app.js
@@ -1,48 +1,13 @@
-//Problem: We need a simple way to look at a user's badge count and JavaScript points
-//Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
-var http = require("http");
-var username = "chalkers";
+var profile = require("./profile.js"); 
 
-function printMessage(username, badgeCount, points) {
-  var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
-  console.log(message);
-}
-
-//// Print out error message ////
-function printError (error) {
-	  console.error(error.message);
-
-}
-
-
-//Connect to the API URL (http://teamtreehouse.com/username.json)
-var request = http.get("http://teamtreehouse.com/" + username + ".json", function(response){
-  var body = "";
-  //Read the data
-  response.on("data", function (chunk) {
-  	body += chunk;
-  });
-  //Parse the data
-  //Print the data
-});
-
-response.on("end", function(){
-	var profile = JSON.parse(body);
-	printMessage(username, profile.badges.length, profile.points.JavaScript);
-  console.error(error.message);
-});
-
-//// Connection Error //// 
-request.on("end", printError);
+// var users = ["chalkers", "matthewsaul", "edennaftali", "davemcfarland"];
 
 
 
+console.log(process.argv);
 
+var users = process.argv.splice(2);
 
+console.log(users);
 
-
-
-
-
-
-
+users.forEach(profile.get);
